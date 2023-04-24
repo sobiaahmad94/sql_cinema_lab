61145cc33fe6d2fb5d62470efd0338df7bcb6d5c
sobiaahmad94 <sobiaahmad94@icloud.com>
2023-04-24 16:08:25 +0100

    completed

---
+++ queries.sql
@@ -0,0 +1,57 @@
+-- Running the people and movies table:
+-- SELECT * FROM people;
+-- SELECT * FROM movies;
+
+-- Question 1:
+-- SELECT * FROM movies;
+
+-- -- Question 2: 
+-- SELECT name FROM people;
+
+-- -- Question 3: 
+-- UPDATE people SET name = "Krusty The Clown" WHERE name = ' Crusty the Clown';
+
+-- Question 4:
+-- SELECT name FROM people WHERE id = 1;
+
+-- Question 5:
+-- DELETE FROM movies WHERE title = 'Batman Begins';
+
+-- Question 6:
+-- INSERT INTO people (name) VALUES ('Bart Simpson');
+
+-- SELECT * FROM people;
+-- SELECT * FROM movies;
+
+-- DELETE FROM people WHERE name = 'Bart Simpson';
+
+-- INSERT INTO people (name) VALUES ('Bart Simpson');
+
+-- SELECT * FROM people;
+-- SELECT * FROM movies;
+
+-- Question 7:
+-- DELETE FROM people WHERE name = 'Eric Cartman';
+-- SELECT * FROM people;
+-- SELECT * FROM movies;
+
+-- Question 8:
+-- INSERT INTO movies (title) VALUES ('Avengers: Infinity War');
+-- SELECT * FROM people;
+-- SELECT * FROM movies;
+
+-- Question 9:
+-- 8.45
+-- UPDATE movies SET show_time = '20.45' WHERE title = 'Iron Man 3';
+
+-- SELECT * FROM people;
+-- SELECT * FROM movies;
+
+-- DELETE FROM people WHERE name IN ('Selma Bouvier', 'Patty Bouvier');
+
+-- Extension question:
+SELECT * FROM people;
+SELECT * FROM movies;
+
+
+
